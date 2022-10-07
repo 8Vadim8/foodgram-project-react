786bbeaa04fc732969629245f18921c4f50c7ea4
Vadim Evlentev <vadim-evl@yandex.ru>
2022-10-07 17:30:53 +0300

    add admin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ingredient, Recipe, RecipeIngredient, Tag
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "author",
+    )
+    list_filter = (
+        "name",
+        "author",
+        "tags",
+    )
+    empty_value_display = "-пусто-"
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "measurement_unit",
+    )
+    list_filter = ("name",)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "color",
+    )
+    list_filter = ("name",)
+
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ("recipe", "ingredient", "amount")
